3ce8530faaeafd891e33bacb3aeb3abc9086f196
CezarBaluta <cezar.baluta@yahoo.com>
2024-02-20 12:27:21 +0200

    mongodb connection established

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 
 app.get("/api", (req, res) => {
   res.json({ users: ["user1", "user2", "user3"] });
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+
+const CONNECTION_URL =
+"mongodb+srv://" +
+process.env.MONGO_ADMIN +
+":" +
+process.env.MONGO_PASSWORD +
+"@cluster0.0zr8trf.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+)
+.catch((error) => console.log(error.message));
+
