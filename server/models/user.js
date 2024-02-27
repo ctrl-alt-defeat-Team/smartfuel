55f36ef6bfcbcfd289b202ab87585caf97452c54
CezarBaluta <cezar.baluta@yahoo.com>
2024-02-28 00:54:11 +0200

    user schema update

--- server/models/user.js
+++ server/models/user.js
@@ -4,6 +4,9 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
+  profilePicURL: String,
+  family: Number,
+  intolerance:[],
 });
 
 userSchema.plugin(passportLocalMongoose, { usernameField: 'username' });
