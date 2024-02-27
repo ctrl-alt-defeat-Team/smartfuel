eef2cfb44b974b8bbec2180ca3eac7d9d9c507fa
calinstefan025 <clavramoniu@gmail.com>
2024-02-27 20:26:37 +0200

    main landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import AuthContainer from "./components/authContainer";
 import Navbar from "./components/navbar";
+import Landing from "./components/landing";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [login, setLogin] = useState(false);
 
   const handleLogin = () => {
-    setLoggedIn(true);
+    setLogin(true);
   };
 
   return (
     <div className="screen-body">
-      <Navbar onLogin={handleLogin} loggedIn={loggedIn} />
+      <Navbar onLogin={handleLogin} loggedIn={login} />
       <div className="main-container">
-        {loggedIn ? <AuthContainer /> : null}
+        {login ? <AuthContainer /> : <Landing />}
       </div>
     </div>
   );
