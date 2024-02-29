5b4e799f96157e6d5995895d26c4a52618220d80
CezarBaluta <cezar.baluta@yahoo.com>
2024-02-29 13:11:23 +0200

    qrModal v1

--- client/src/components/qrModal.js
+++ client/src/components/qrModal.js
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
-import '../styles/Modal.css'; // Import CSS for modal styling
-
-function Modal() {
-    return (
-        <div>
-                <div className="modal">
-                    <div className="modal-content">
-                        <p>This is the content of the modal.</p>
-                    </div>
-                </div>
-        </div>
-    );
+// QRModal.js
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import "../styles/Modal.css"; 
+function QRModal({ showModal, closeModal }) {
+  return (
+    <Modal show={showModal} onHide={closeModal} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>QR Code Scan</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* Your QR code scanning content goes here */}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={closeModal}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
-export default Modal;
+export default QRModal;
