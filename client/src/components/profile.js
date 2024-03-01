b611d12e1d469c0b17bda28b5d6a571a7d4a8b93
alinandrei2004 <145998080+alinandrei2004@users.noreply.github.com>
2024-03-01 09:20:32 +0200

    profile allergens

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -51,6 +51,68 @@ function Profile() {
                         </form>
                     </div>
                 <div className="form-group">
+                    <fieldset id="allergens">
+                    <legend>Select allergens:</legend>
+
+                    <div>
+                        <input type="checkbox" id="gluten" name="drone" value="gluten" checked />
+                        <label for="gluten">Gluten</label>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" id="milk" name="drone" value="milk" />
+                        <label for="milk">Milk</label>
+                    </div>
+
+                    <div>
+                        <input type="checkbox" id="eggs" name="drone" value="eggs" />
+                        <label for="eggs">Eggs</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="nuts" name="drone" value="nuts" />
+                        <label for="nuts">Nuts</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="peanuts" name="drone" value="peanuts" />
+                        <label for="peanuts">Peanuts</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="sesame" name="drone" value="sesame" />
+                        <label for="sesame">Sesame seads</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="soybeans" name="drone" value="soybeans" />
+                        <label for="soybeans">Soybeans</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="celery" name="drone" value="celery" />
+                        <label for="celery">Celery</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="mustard" name="drone" value="mustard" />
+                        <label for="mustard">Mustard</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="lupin" name="drone" value="lupin" />
+                        <label for="lupin">Lupin</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="molluscs" name="drone" value="molluscs" />
+                        <label for="molluscs">Molluscs</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="fish" name="drone" value="fish" />
+                        <label for="fish">Fish</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="crustaceans" name="drone" value="crustaceans" />
+                        <label for="crustaceans">Crustaceans</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="sulphur" name="drone" value="sulphur" />
+                        <label for="sulphur">Sulphur dioxide and sulphites</label>
+                    </div>
+                    </fieldset>
                 </div>     
             </div>
             <div>
