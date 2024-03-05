67bff83478e8880dd8be282d4b2c2fe046269492
CezarBaluta <cezar.baluta@yahoo.com>
2024-03-05 03:42:48 +0200

    debounceDetection implementation + error log bug fix

--- client/src/components/Scanner.js
+++ client/src/components/Scanner.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import config from "./config.json";
 import Quagga from "quagga";
 import searchProduct from "../functions/searchProduct";
+import _debounce from "lodash/debounce";
 
 import "../styles/Scanner.css"
 
@@ -20,17 +21,26 @@ const Scanner = props => {
       }
     });
 
-    Quagga.onDetected(detected);
-  }, [detectionOccurred]);
+    Quagga.onDetected(debouncedDetected);
+  }, []);
+
 
   const detected = async(result) => {
+    Quagga.pause();
+    //console.log("camera stopped");
     if (!detectionOccurred) {
-      console.log(result.codeResult.code);
+      setDetectionOccurred(true);
+      //console.log(result.codeResult.code);
       if(result.codeResult.code){
         try {
-          const productData = await searchProduct(result.codeResult.code);
-          console.error("barcode", result.codeResult.code, "not found");
-          if(productData == -1 || productData.product_name == null){
+          const productData = await searchProduct("barcode",result.codeResult.code);
+        //  console.log(productData);
+          if(productData == undefined ||
+            productData == -1  ||
+            productData.product_name == null){
+            console.error("barcode", result.codeResult.code, "not found");
+            setDetectionOccurred(false);
+            Quagga.start();
             return;
           }
         }catch (error) {
@@ -38,10 +48,14 @@ const Scanner = props => {
         }
       }
       setDetectionOccurred(true);
+      console.log("barcode detected", result.codeResult.code);
       onDetected(result.codeResult.code);
       Quagga.stop();
     }
+    else return;
   };
+
+  const debouncedDetected = _debounce(detected, 200, { leading: true, trailing: false });
 
   return (
     <div id="interactive" className="viewport" />
