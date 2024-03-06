390c0ab2d9a9ddb1dc48bd9f5d320e5183a0d0a8
CezarBaluta <cezar.baluta@yahoo.com>
2024-03-06 15:20:14 +0200

    navbarButtons fix + revert merge #24

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,38 @@ import Profile from "./components/profile";
 import Cart from "./components/cart";
 
 function App() {
-  const [login, setLogin] = useState(false);
+  const [showLogin, setshowLogin] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const [showProfile, setShowProfile] = useState(false);
   const [showCart, setShowCart] = useState(false);
 
   const handleLogin = () => {
-    setLogin(true);
+    setshowLogin(true);
+    setShowProfile(false);
+    setShowCart(false); 
   };
 
   const handleProfileClick = () => {
-    setShowProfile(!showProfile);
+    setShowProfile(true);
+    setShowCart(false);
+    setshowLogin(false);
   }
 
   const handleCartClick = () => {
-    setShowCart(!showCart);
+    setShowCart(true);
+    setShowProfile(false);
+    setshowLogin(false); 
   }
 
   return (
     <div className="screen-body">
-      <Navbar onLogin={handleLogin} loggedIn={login} onProfileClick={handleProfileClick} showProfile={showProfile} onCartClick={handleCartClick} showCart={showCart} />
+      <Navbar onLogin={handleLogin} loggedIn={loggedIn} showLogin={showLogin} onProfileClick={handleProfileClick} showProfile={showProfile} onCartClick={handleCartClick} showCart={showCart} />
       <div className="main-container">
-        {login ? <AuthContainer /> : showProfile ? <Profile /> : showCart ? <Cart /> : <Landing />} 
+        {(showLogin) && <AuthContainer />}
+        {(showProfile) && <Profile /> }
+        {(showCart) && <Cart />}
+        {(showCart == false && showLogin == false && showProfile == false) 
+        && <Landing />} 
       </div>
     </div>
   );
