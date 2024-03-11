3ce60da17dfc2e74ddfd365fec1203fbb823d10e
alinandrei2004 <145998080+alinandrei2004@users.noreply.github.com>
2024-03-11 17:49:57 +0200

    cart mobile

--- client/src/components/cart.js
+++ client/src/components/cart.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "../styles/CartPage.css";
 import CartProduct from "./cartProduct"; // Import CartProduct component
 import Button from "react-bootstrap/Button";
+import "../styles/Mobile.css";
 
 function Cart({setShowCart}) {
     const closeButtonClick = () => {
