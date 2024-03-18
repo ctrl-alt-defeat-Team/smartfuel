31ef6042bc59784781e616446da7d33b3ce3fa6f
calinstefan025 <clavramoniu@gmail.com>
2024-03-18 20:25:45 +0200

    Update dashSubmissions.js

--- client/src/components/dashSubmissions.js
+++ client/src/components/dashSubmissions.js
@@ -24,6 +24,7 @@ function DashSubmissions() {
           throw new Error("Failed to fetch data");
         }
         const data = await response.json();
+        console.log("Fetched data", data);
         setSubmitedItems(data);
       } catch (error) {
         console.error("Error fetching data:", error);
