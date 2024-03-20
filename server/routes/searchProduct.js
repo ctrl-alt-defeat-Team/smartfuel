c68f06f27f59c65b0c26922e1581ed79191a3a89
CezarBaluta <cezar.baluta@yahoo.com>
2024-03-20 19:30:53 +0200

    search product in DB implementation

--- server/routes/searchProduct.js
+++ server/routes/searchProduct.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var router = express.Router();
 const fetch = require('node-fetch');
+var Product = require('../models/product.js');
 
 router.get('/barcode/:productQR', async (req, res) => {
     //console.log('searching product');
     //console.log(req.params.productQR);
     const productQR = req.params.productQR;
     try {
+        var response2 = await Product.findOne({barcode: productQR});
+        if(response2){
+            response2.allergens_tags = [""];
+            res.status(200).json(response2);
+        }
         const response = await fetch(`https://world.openfoodfacts.org/api/v2/product/${productQR}`);
         const data = await response.json();
         res.status(200).json(data.product);
@@ -19,12 +25,23 @@ router.get('/barcode/:productQR', async (req, res) => {
 router.get('/name/:name', async (req, res) => {
     console.log('searching product');
     console.log(req.params.name);
+  
     const productName = req.params.name;
     try {
+        const response2 = await Product.find({product_name: productName});
+        console.log(response2);
         const response = await fetch(`https://world.openfoodfacts.org/cgi/search.pl?search_terms=${productName}&search_simple=1&action=process&json=1`);
-        console.log(response);
+        //console.log(response);
+        const v =[];
+
         const data = await response.json();
-        res.status(200).json(data.products);
+        response2.map((product) => {
+            product.allergens_tags = [""];
+            v.push(product);
+        });
+        console.log("response2:", response2);
+        v.push(data.products);
+        res.status(200).json(v);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: error });
